724952bca6dfc6309205dd9728c9703979ca6243
miguel <miguel3.santos3@gmail.com>
2024-04-26 01:24:01 -0300

    Exemplo de variaveis e metodos

--- java-anatomia-classes/src/Main.java
+++ java-anatomia-classes/src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String primeiroNome = "Miguel";
+        String segundoNome = "Maxmillian";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto ( String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
     }
 }
