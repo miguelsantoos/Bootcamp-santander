88bacf514aad6b40048dca45960df0e16e4e331d
miguel <miguel3.santos3@gmail.com>
2024-04-29 21:09:49 -0300

    Testes de alguns exercicios

--- java-anatomia-classes/src/edu/miguel/tema/operadores/Main.java
+++ java-anatomia-classes/src/edu/miguel/tema/operadores/Main.java
@@ -4,6 +4,23 @@ package edu.miguel.tema.operadores;
 public class Main {
 
     public static void main(String[] args) {
+        int numero = 0;
+        String sim;
+        while (numero < 11) {
+            sim = "sim";
+
+            if (sim.equals("nao")){
+                numero++;
+                System.out.println(numero);
+            }else {
+                break;
+            }
+        }
+
+        int resultado = numero < 11 ? numero++ : 0;
+        System.out.println(resultado);
+        }
+
 
     }
 }
