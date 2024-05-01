10344beb59e218ea9e593276e0e8b635a936ae6c
miguel <miguel3.santos3@gmail.com>
2024-05-01 16:54:19 -0300

    Seunda parte do estudo de metodos: criado alguns exemplos

--- java-anatomia-classes/src/edu/miguel/tema/metodos/SmartTv.java
+++ java-anatomia-classes/src/edu/miguel/tema/metodos/SmartTv.java
@@ -6,6 +6,26 @@ public class SmartTv {
     int canal = 1;
     int volume = 25;
 
+    public void mudarCanal(int novoCanal) {
+        canal = novoCanal;
+    }
+
+    public void diminuirCanal() {
+        canal = --canal;
+    }
+
+    public void aumentarCanal() {
+        canal = ++canal;
+    }
+
+    public void diminuirVolume() {
+        volume = --volume;
+    }
+
+    public void aumentarVolume() {
+        volume = ++volume;
+    }
+
     public void ligar() {
         ligada = true;
     }
@@ -13,5 +33,6 @@ public class SmartTv {
     public void desligar() {
         ligada = false;
     }
+
 
 }
--- java-anatomia-classes/src/edu/miguel/tema/metodos/Usuario.java
+++ java-anatomia-classes/src/edu/miguel/tema/metodos/Usuario.java
@@ -11,5 +11,21 @@ public class Usuario {
 
         smartTv.ligar();
         System.out.println("NOVA AÇAO - TV ligada: " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("NOVA AÇAO - TV ligada: " + smartTv.ligada);
+
+        smartTv.mudarCanal(13);
+        System.out.println("NOVA AÇAO - Qual canal: " + smartTv.canal);
+        smartTv.aumentarCanal();
+        System.out.println("NOVA AÇAO - Qual canal: " + smartTv.canal);
+        smartTv.diminuirCanal();
+        System.out.println("NOVA AÇAO - Qual canal: " + smartTv.canal);
+
+        smartTv.aumentarVolume();
+        System.out.println("NOVA AÇAO - Qual volume: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        System.out.println("NOVA AÇAO - Qual volume: " + smartTv.volume);
+
+
     }
 }
