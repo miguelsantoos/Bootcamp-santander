247d06ba02b800c21e59a2953d51e6611881799a
miguel <miguel3.santos3@gmail.com>
2024-05-01 19:36:18 -0300

    Fazendo a interação com o usuario atraves do terminal da IDE

--- java-anatomia-classes/src/edu/miguel/tema/teriminalargumentos/AboutMe.java
+++ java-anatomia-classes/src/edu/miguel/tema/teriminalargumentos/AboutMe.java
@@ -1,12 +1,28 @@
 package edu.miguel.tema.teriminalargumentos;
 
+import java.util.Scanner;
+
 public class AboutMe {
 
     public static void main(String[] args) {
-        String nome = args[0];
-        String sobrenome = args[1];
-        String idade = args[3];
-        String alturra = args[4];
+        String nome;
+        String sobrenome;
+        String idade;
+        String alturra;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome; ");
+        nome = scanner.next();
+
+        System.out.println("Digite seu sobrenome; ");
+        sobrenome = scanner.next();
+
+        System.out.println("Digite sua idade; ");
+        idade = scanner.next();
+
+        System.out.println("Digite sua altura; ");
+        alturra = scanner.next();
 
         System.out.println("Olá, me chamo " + nome + sobrenome);
         System.out.println("Tenho de " + idade + " idade");
