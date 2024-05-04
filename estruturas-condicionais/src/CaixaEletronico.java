d29ddbf3be59f587cc6660c5b403a18cb95347be
miguel <miguel3.santos3@gmail.com>
2024-05-04 15:37:58 -0300

    Implemtanso condições compostas

--- estruturas-condicionais/src/CaixaEletronico.java
+++ estruturas-condicionais/src/CaixaEletronico.java
@@ -2,14 +2,15 @@ public class CaixaEletronico {
     public static void main(String[] args) {
 
         double saldo = 100.00;
-        double valorSolicitado = 50.00;
+        double valorSolicitado = 250.00;
 
-        if (saldo > valorSolicitado) {
+        if (saldo >= valorSolicitado) {
 
             saldo = saldo - valorSolicitado;
+            System.out.println(saldo);
 
+        }else {
+            System.out.println("Seu saldo é insulficiente");
         }
-        
-        System.out.println(saldo);
     }
 }
