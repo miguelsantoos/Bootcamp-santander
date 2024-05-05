c11bf421f163266de8b877d25e0c4021f1102f31
miguel <miguel3.santos3@gmail.com>
2024-05-05 16:20:59 -0300

    Testes

--- estruturas-repeticao/src/ExemploFor.java
+++ estruturas-repeticao/src/ExemploFor.java
@@ -5,6 +5,9 @@ public class ExemploFor {
 
         // FOR tem siguinificado de PARA...
         for (contagem = 1; contagem <= 10; contagem++){
+            if (contagem % 2 == 0 )
+                System.out.println(contagem);
+
             System.out.println("CONTAGEM " + contagem);
         }
     }
