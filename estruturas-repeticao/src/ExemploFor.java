29b85fcc70757cd1f5d9092c1e0b621bf423a8d1
miguel <miguel3.santos3@gmail.com>
2024-05-05 12:14:23 -0300

    Estrutura de repetição usando array

---
+++ estruturas-repeticao/src/ExemploFor.java
@@ -0,0 +1,11 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+
+        int contagem = 1;
+
+        // FOR tem siguinificado de PARA...
+        for (contagem = 1; contagem <= 10; contagem++){
+            System.out.println("CONTAGEM " + contagem);
+        }
+    }
+}
