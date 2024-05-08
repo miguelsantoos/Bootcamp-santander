8966bf97a55d0b43e06271d0b90737240533d91b
miguel <miguel3.santos3@gmail.com>
2024-05-08 12:03:38 -0300

    Mostrando como funciona a interação de classes

--- visibilidade-recursos/src/lanchonete/atendimento/Atendente.java
+++ visibilidade-recursos/src/lanchonete/atendimento/Atendente.java
@@ -1,4 +1,4 @@
-package lanchonete;
+package lanchonete.atendimento;
 
 public class Atendente {
     public void servindoMesa() {
@@ -6,15 +6,25 @@ public class Atendente {
         System.out.println("SERVINDO A MESA");
     }
     public void pegarLancheCozinha() {
+
         System.out.println("PEGANDO O LANCHE NA COZINHA");
     }
+
+
     public void receberPagamento() {
+
         System.out.println("RECEBENDO PAGAMENTO");
     }
-    public void trocarGas() {
+
+
+    void trocarGas() {
+
         System.out.println("ATENDENTE TROCANDO O GÁS");
     }
+
+
     public void pegarPedidoBalcao() {
+
         System.out.println("PEGANDO O PEDIDO NO BALCÃO");
     }
 }
--- visibilidade-recursos/src/lanchonete/atendimento/cozinha/Cozinheiro.java
+++ visibilidade-recursos/src/lanchonete/atendimento/cozinha/Cozinheiro.java
@@ -1,51 +1,88 @@
-package lanchonete;
+package lanchonete.atendimento.cozinha;
+
+import lanchonete.atendimento.Atendente;
 
 public class Cozinheiro {
+
+
     //pode ser default
     public void adicionarLancheNoBalcao() {
+        prepararLanche();
         System.out.println("ADICIONANDO LANCHE NATURAL HAMBÚRGUER NO BALCÃO");
     }
+
+
     //pode ser default
     public void adicionarSucoNoBalcao() {
+        prepararVitamina();
         System.out.println("ADICIONANDO SUCO NO BALCÃO");
     }
+
+
     //pode ser default
     public void adicionarComboNoBalcao() {
-        adicionarLancheNoBalcao();
-        adicionarSucoNoBalcao();
+        prepararCombo();
+        System.out.println("ADICIONANDO COMBO NO BALCÃO");
     }
-    public void prepararLanche() {
+
+
+    private void prepararLanche() {
+
         System.out.println("PREPARANDO LANCHE TIPO HAMBÚRGUER");
     }
-    public void prepararVitamina() {
+
+
+    private void prepararVitamina() {
+
         System.out.println("PREPARANDO SUCO");
     }
-    public void prepararCombo() {
+
+
+    private void prepararCombo() {
+        System.out.println("PREPARANDO COMBO");
         prepararLanche();
         prepararVitamina();
     }
-    public void selecionarIngredientesLanche() {
+
+
+    private void selecionarIngredientesLanche() {
+
         System.out.println("SELECIONADO O PÃO, SALADA, OVO E CARNE");
     }
-    public void selecionarIngredientesVitamina() {
+
+
+    private void selecionarIngredientesVitamina() {
+
         System.out.println("SELECIONADO FRUTA, LEITE E SUCO");
     }
-    public void lavarIngredientes() {
+
+
+    private void lavarIngredientes() {
+
         System.out.println("LAVANDO INGREDIENTES");
     }
-    public void baterVitaminaLiquidificador() {
+
+
+    private void baterVitaminaLiquidificador() {
+
         System.out.println("BATENDO VITAMINA NO LIQUIDIFICADOR");
     }
-    public void fritarIngredientesLanche() {
+
+
+    private void fritarIngredientesLanche() {
+
         System.out.println("FRITANDO A CARNE E OVO PARA O HAMBÚRGUER");
     }
-    public void pedirParaTrocarGas(Atendente meuAmigo) {
+
+
+    private void pedirParaTrocarGas(Almoxarife meuAmigo) {
+
         meuAmigo.trocarGas();
     }
-    public void pedirParaTrocarGas(Almoxarife meuAmigo) {
-        meuAmigo.trocarGas();
-    }
-    public void pedirIngredientes(Almoxarife almoxarife) {
+
+
+    void pedirIngredientes(Almoxarife almoxarife) {
+
         almoxarife.entregarIngredientes();
     }
 }
