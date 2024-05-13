3e8f81985befd8463a69a82c972ccccc55f095ff
miguel <miguel3.santos3@gmail.com>
2024-05-13 14:36:53 -0300

    Initial commit

---
+++ orientado-objeto/src/MSNMensagem.java
@@ -0,0 +1,17 @@
+public class MSNMensagem {
+    public void enviarMensagem() {
+        System.out.println("Enviando mensagem");
+    }
+
+    public void recebrMensagem() {
+        System.out.println("Recebendo mensagem");
+    }
+
+    private void validarConectadoInternet() {
+        System.out.println("Verificando se estar conectado na internet");
+    }
+
+    private void salvarHistoricoMensagem() {
+        System.out.println("Salvando historico de mensagem");
+    }
+}
